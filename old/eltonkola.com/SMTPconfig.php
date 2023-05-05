b784519f71ae4c2df446c0c3414a369160258d4f
Elton Kola <eltonkola@gmail.com>
2023-05-05 09:58:27 -0400

    remove old files
    use zip for them

--- old/eltonkola.com/SMTPconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Server Address
-$SmtpServer="mail.eltonkola.com";
-$SmtpPort="26";//25
-$SmtpUser="eltonkol";
-$SmtpPass="$$ElidonaKola81$$";
-
-?>
--- old/eltonkola.com/m/old/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-	header( "Location: http://www.eltonkola.com/old" );
-	exit;
-?>
