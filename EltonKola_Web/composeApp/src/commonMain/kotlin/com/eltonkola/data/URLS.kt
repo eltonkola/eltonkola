64e17ee15b0a33030e94a81edf8dc3fd854f1774
Elton Kola <eltonkola@gmail.com>
2025-05-30 23:52:10 -0400

    asd
    asd

--- EltonKola_Web/composeApp/src/commonMain/kotlin/com/eltonkola/data/URLS.kt
+++ EltonKola_Web/composeApp/src/commonMain/kotlin/com/eltonkola/data/URLS.kt
@@ -6,11 +6,11 @@ object URLS {
     const val PROFILE_GITHUB = "https://github.com/eltonkola"
     const val PROFILE_PLAYSTORE = "https://play.google.com/store/apps/dev?id=6135815754208595340"
 
-    const val DOWNLOAD_ANDROID = "https://eltonkola.com/downloads/app.apk"
-    const val DOWNLOAD_LINUX = "https://eltonkola.com/downloads/app.deb"
-    const val DOWNLOAD_WINDOWS = "https://eltonkola.com/downloads/app.msi"
-    const val DOWNLOAD_MACOS = "https://eltonkola.com/downloads/app.dmg"
-    const val DOWNLOAD_IPHONE = "https://eltonkola.com/downloads/app.ipa"
+    const val DOWNLOAD_ANDROID = "https://github.com/eltonkola/eltonkola/releases/download/v1.0.14/ek-14.apk"
+    const val DOWNLOAD_LINUX = "https://github.com/eltonkola/eltonkola/releases/download/v1.0.13/ek-13-amd64.deb"
+    const val DOWNLOAD_WINDOWS = "https://github.com/eltonkola/eltonkola/releases/download/v1.0.13/ek-13.msi"
+    const val DOWNLOAD_MACOS = "https://github.com/eltonkola/eltonkola/releases/download/v1.0.13/ek-13.dmg"
+    const val DOWNLOAD_IPHONE = "https://github.com/eltonkola/eltonkola/releases/download/v1.0.14/ek-14.ipa"
 
     const val DOWNLOAD_RESUME = "https://eltonkola.com/downloads/Resume_EltonKola_2025.pdf"
     const val LINK_WEB_LITE = "https://eltonkola.com"
