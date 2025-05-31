133c992e26e8ce820a801a7634ac7f4290ca2610
Elton Kola <eltonkola@gmail.com>
2025-05-30 20:52:28 -0400

    update urkls

--- EltonKola_Web/composeApp/src/commonMain/kotlin/com/eltonkola/data/URLS.kt
+++ EltonKola_Web/composeApp/src/commonMain/kotlin/com/eltonkola/data/URLS.kt
@@ -6,11 +6,11 @@ object URLS {
     const val PROFILE_GITHUB = "https://github.com/eltonkola"
     const val PROFILE_PLAYSTORE = "https://play.google.com/store/apps/dev?id=6135815754208595340"
 
-    const val DOWNLOAD_ANDROID = "https://eltonkola.com/downloads/ek.apk"
-    const val DOWNLOAD_LINUX = "https://eltonkola.com/downloads/ek-amd64.deb"
-    const val DOWNLOAD_WINDOWS = "https://eltonkola.com/downloads/ek.msi"
-    const val DOWNLOAD_MACOS = "https://eltonkola.com/downloads/ek.dmg"
-    const val DOWNLOAD_IPHONE = "https://eltonkola.com/downloads/ek.ipa"
+    const val DOWNLOAD_ANDROID = "https://eltonkola.com/downloads/app.apk"
+    const val DOWNLOAD_LINUX = "https://eltonkola.com/downloads/app.deb"
+    const val DOWNLOAD_WINDOWS = "https://eltonkola.com/downloads/app.msi"
+    const val DOWNLOAD_MACOS = "https://eltonkola.com/downloads/app.dmg"
+    const val DOWNLOAD_IPHONE = "https://eltonkola.com/downloads/app.ipa"
 
     const val DOWNLOAD_RESUME = "https://eltonkola.com/downloads/Resume_EltonKola_2025.pdf"
     const val LINK_WEB_LITE = "https://eltonkola.com"
